81aef7bbdb9fd9edd5224ff42718253846c12f2b
Nikola Stojanovski <nikola211223232529321@gmail.com>
2026-01-21 21:34:28 +0100

    redesign of the about page and the navbar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,113 +1,63 @@
+import Link from "next/link";
 import Image from "next/image";
 import heroImage from "../../public/assets/images/about/about-hero-image.jpg";
-import PrimaryButton from "@/components/buttons/PrimaryButton";
-import AnimatedBorder from "@/components/AnimatedBorder";
-import TitleLoop from "@/components/TitleLoop";
 
-import { StaticImageData } from "next/image";
-import testImg1 from "../../public/assets/images/projects/tp1.png";
-import testImg2 from "../../public/assets/images/projects/tp2.png";
-import testImg3 from "../../public/assets/images/projects/tp3.png";
+import { LiaDownloadSolid } from "react-icons/lia";
 
 const page = () => {
-  interface StoryChapter {
-    id: number;
-    year: number;
-    title: string;
-    description: string;
-    images: (string | StaticImageData)[];
-  }
-
-  const myStory: StoryChapter[] = [
-    {
-      id: 1,
-      year: 2022,
-      title: "Self-Taught Beginnings",
-      description:
-        "I started learning how to code on my own, driven by curiosity and a desire to build things from scratch.",
-      images: [testImg1, testImg2, testImg3],
-    },
-    {
-      id: 2,
-      year: 2023,
-      title: "Full-Stack Academy",
-      description:
-        "I enrolled in Semos Education, where I improved my skills and gained hands-on experience with HTML, CSS, JavaScript, Node.js, TypeScript, MongoDB, and React.",
-      images: [testImg1, testImg2, testImg3],
-    },
-    {
-      id: 3,
-      year: 2024,
-      title: "Frontend Developer at Pabau",
-      description:
-        "I launched my career as a frontend developer, improving my skills and learning new technologies like GraphQL, Prisma, and Jest, while collaborating with a professional team.",
-      images: [testImg1, testImg2, testImg3],
-    },
-    {
-      id: 4,
-      year: 2025,
-      title: "Frontend Developer at Thrasker",
-      description:
-        "At Thrasker, I worked with Astro.js and Shopify, contributed to real-world projects, made lasting connections, and took my frontend expertise to the next level.",
-      images: [testImg1, testImg2, testImg3],
-    },
-    {
-      id: 5,
-      year: 2026,
-      title: "Freelance Full-Stack Developer",
-      description:
-        "Now I work as a freelance full-stack developer, creating modern, user-focused web projects and bringing ideas from concept to deployment on and off freelancing platforms.",
-      images: [testImg1, testImg2, testImg3],
-    },
-  ];
-
   return (
     <>
-      <section className="bg-background py-24 lg:py-36">
-        <div className="container mx-auto px-4 sm:px-6">
-          <TitleLoop title="Who I Am and What I Do" />
-
-          <div className="relative grid lg:grid-cols-12 gap-12 pt-12 lg:pt-20">
-            <AnimatedBorder className="absolute top-0 left-0" />
-
-            <div className="lg:col-span-3 relative min-h-72 max-lg:hidden">
-              <Image
-                src={heroImage}
-                alt="hero image"
-                height={800}
-                width={500}
-                className="absolute top-0 left-0 w-2/3 h-2/3 object-cover max-lg:object-top"
-              />
+      <section className="py-24 md:py-36">
+        <div className="container mx-auto xl:max-w-6xl px-6">
+          <div className="flex items-end justify-between md:gap-10 border-b border-border pb-10 md:pb-16">
+            <h1 className="text-foreground max-[370px]:text-4xl text-5xl sm:text-6xl lg:text-7xl xl:text-8xl leading-tight">
+              Who I Am and <br /> What I Do
+            </h1>
+            <div className="max-md:hidden">
+              <Link
+                href={"/"}
+                className="grid grid-cols-[1fr_auto] items-start gap-1"
+              >
+                <div className="flex items-center justify-center bg-muted px-4 h-full w-full">
+                  <LiaDownloadSolid className="text-foreground text-bsae" />
+                </div>
+                <div className="bg-muted py-3 px-6">
+                  <span className="text-foreground text-base">
+                    Download Resume
+                  </span>
+                </div>
+              </Link>
             </div>
-
-            <div className="flex flex-col gap-6 sm:gap-10 xl:gap-20 lg:col-span-8 lg:col-start-5">
-              <h2 className="text-foreground text-2xl sm:text-3xl xl:text-[42px] leading-tight">
-                Hi, I’m Nikola, a full-stack developer with over 3 years of
+          </div>
+          <div className="grid md:grid-cols-12 gap-20 pt-10 md:pt-16">
+            <div className="md:col-span-7">
+              <h2 className="max-[370px]:text-2xl text-2xl lg:text-[32px] md:pb-16">
+                I’m Nikola, a full-stack developer with over 3 years of
                 professional experience based in Skopje, Macedonia.
               </h2>
-              <div className="max-h-72 block lg:hidden">
+              <div className="aspect-video my-10 md:hidden">
                 <Image
                   src={heroImage}
                   alt="hero image"
-                  width={700}
-                  height={400}
+                  width={800}
+                  height={600}
                   className="h-full w-full object-cover object-top"
                 />
               </div>
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-10 xl:gap-20">
+              <div className="flex flex-col gap-6">
                 <div>
-                  <h5 className="text-lg text-foreground/90 pb-2">What I Do</h5>
-                  <p className="text-base text-muted-foreground">
+                  <h5 className="text-foreground/80 text-lg pb-2">What I Do</h5>
+                  <p className="text-base text-muted-foreground max-w-[90%]">
                     I build modern, scalable web applications using JavaScript
                     and TypeScript, with a focus on React, Next.js, and Node.js,
                     prioritizing performance and maintainable code.
                   </p>
                 </div>
                 <div>
-                  <h5 className="text-lg text-foreground/90 pb-2">
+                  <h5 className="text-foreground/80 text-lg pb-2">
                     Beyond Code
                   </h5>
-                  <p className="text-base text-muted-foreground">
+                  <p className="text-base text-muted-foreground max-w-[90%]">
                     Outside of development, I like staying active and keeping a
                     good balance. I enjoy watching sports, going to the gym,
                     watching movies and TV shows, and spending time with
@@ -115,49 +65,31 @@ const page = () => {
                   </p>
                 </div>
               </div>
-
-              <div className="max-lg:pt-6">
-                <PrimaryButton title="Let's  WorkTogether" link="/" />
+              <div className="md:hidden pt-10">
+                <Link
+                  href={"/"}
+                  className="grid grid-cols-[1fr_auto] items-start gap-1 w-fit"
+                >
+                  <div className="flex items-center justify-center bg-muted px-4 h-full">
+                    <LiaDownloadSolid className="text-foreground text-bsae" />
+                  </div>
+                  <div className="bg-muted py-3 px-6">
+                    <span className="text-foreground text-base">
+                      Download Resume
+                    </span>
+                  </div>
+                </Link>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-      <section className="pb-24 lg:pb-36">
-        <div className="container mx-auto px-4 sm:px-6">
-          <div className="flex items-start justify-between border-t border-border pt-4">
-            <span className="text-foreground text-base">My Story</span>
-            <span className="text-foreground text-base">2022 - 2025</span>
-          </div>
-          <div className="flex flex-col gap-10 pt-36">
-            {myStory.map((chapter) => (
-              <div
-                key={chapter.id}
-                className="grid grid-cols-12 border-b border-border pb-10"
-              >
-                <div className="col-span-3">
-                  <span className="text-foreground text-lg">
-                    {chapter.year}
-                  </span>
-                </div>
-                <div className="col-span-5">
-                  <h5 className="text-foreground text-3xl">{chapter.title}</h5>
-                </div>
-                <div className="grid grid-cols-3 gap-3 col-span-3 col-end-13">
-                  {chapter.images.map((image, index) => (
-                    <div key={index}>
-                      <Image
-                        src={image}
-                        alt="image"
-                        width={100}
-                        height={100}
-                        className="h-full w-full object-cover"
-                      />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
+            <div className="relative col-span-3 lg:col-end-13 max-md:hidden">
+              <Image
+                src={heroImage}
+                alt="about image"
+                width={500}
+                height={500}
+                className="absolute top-0 right-0 w-full h-2/3 object-cover"
+              />
+            </div>
           </div>
         </div>
       </section>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav>
-      <div className="container mx-auto px-6">
+      <div className="container mx-auto xl:max-w-6xl px-6">
         <div className="flex items-center justify-between gap-6 py-10">
           <Link href="/">Nikola</Link>
-          <Link href="/about">About</Link>
-          <Link href="/projects">Projects</Link>
-          <Link href="/">Socials</Link>
+          <div className="flex items-center gap-10">
+            <Link href="/about">About</Link>
+            <Link href="/projects">Projects</Link>
+            <Link href="/">Socials</Link>
+          </div>
         </div>
       </div>
     </nav>
